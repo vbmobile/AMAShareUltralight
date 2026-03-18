da50b4087a75564a95cebeea10683ae75646e6a0
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-03-18 15:38:02 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,9 +14,14 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "AmaShareUltralight",
+            name: "AmaShareUltralight-1.0.0-rc01",
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/AmaShareUltralight/AmaShareUltralight-1.0.0-rc01.zip",
             checksum: "7d501bda28e855d4fa8a6e6d55cbce2c5bb633bb776a69d473b335987e302f36"
+        )
+        .target(
+            name: "AmaShareUltralight",
+            dependencies: ["AmaShareUltralight-1.0.0-rc01"],
+            path: "Sources"
         )
     ]
 )
