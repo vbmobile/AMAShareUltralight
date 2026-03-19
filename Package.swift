a75fedd37df87e4079ac393a3835ca9a8d15f725
ricardopatriciodossantos <ricardo.dossantos+amadeus@amadeus.com>
2026-03-19 17:26:05 +0000

    Chaned aproach

--- Package.swift
+++ Package.swift
@@ -13,28 +13,24 @@ let package = Package(
         )
     ],
     targets: [
-        // ✅ MODULE NAME MUST MATCH EXACTLY `AMADocModeliOS`
-        .binaryTarget(
+        /*.binaryTarget(
             name: "AMADocModeliOS",
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocModeliOS/mdi-mob-sdk-doc-model-ios-1.0.0-rc03.zip",
             checksum: "721be7c6762ed45c25907faa786764c27529f0a15476dece5099ffbf654ba2d6"
-        ),
-
+        ),*/
         .binaryTarget(
             name: "UltralightFramework",
             path: "Frameworks/UltralightFramework.xcframework"
         ),
-
         .binaryTarget(
             name: "AMAShareUltralightBin",
             path: "Frameworks/AMAShareUltralight.xcframework"
         ),
-
         .target(
             name: "AMAShareUltralight",
             dependencies: [
                 "AMAShareUltralightBin",
-                "AMADocModeliOS",
+                //"AMADocModeliOS",
                 "UltralightFramework"
             ],
             path: "Sources/AMAShareUltralight"
