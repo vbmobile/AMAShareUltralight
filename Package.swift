7e46dbcd11e0fd02a74038e765ee4a548ce3443a
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-03-19 14:15:29 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
             checksum: "1e25250fb3ad60eeedad635f5b4c9149b4f127f164816e9624bd6df24905cdff"
         ),
         .target(
-            name: "AMAShareUltralightBin",
-            dependencies: ["AMAShareUltralight"],
+            name: "AMAShareUltralight",
+            dependencies: ["AMAShareUltralightBin"],
             path: "Sources"
         )
     ]
