9e2d8a407b1f452a0982d3691d896e3ce8cefe2c
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-03-19 13:26:48 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "AMAShareUltralight",
-    defaultLocalization: "en",
     platforms: [
         .iOS(.v15)
     ],
@@ -15,15 +14,14 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "mdi-mob-ultralight",
+            name: "AMAShareUltralight",
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAShareUltralight/AMAShareUltralight-1.0.0-rc05.zip",
             checksum: "1fb07c79d1fadf8d052927260a56e6d1fb34e9c0e2dc1ff2edb0e823782a2046"
         ),
         .target(
             name: "AMAShareUltralight",
-            dependencies: ["mdi-mob-ultralight"],
+            dependencies: ["AMAShareUltralight"],
             path: "Sources"
         )
     ]
 )
-
