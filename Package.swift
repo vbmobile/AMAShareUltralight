28d91b27544418dffce1a82a872e720a7d1e72bc
MobileID <123370216+vbmobile@users.noreply.github.com>
2026-04-16 11:19:35 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vbmobile/AMADocModeliOS",
-                 exact: "1.0.0-rc20"),
+                 exact: "1.0.0-rc23"),
         .package(url: "https://github.com/vbmobile/ultralight-native-sdk",
                  exact: "2.6.141")
     ],
     targets: [
         .binaryTarget(
             name: "AMAShareUltralight",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAShareUltralight/AMAShareUltralight-1.0.0-rc22.zip",
-            checksum: "7013bcc473917f34cb73ea55b40b542a63d74bd5661cab1816270acc3883b3cd"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMAShareUltralight/AMAShareUltralight-1.0.0-rc23.zip",
+            checksum: "e9b5f0613be6325398173e333ae25e65a8b7c4bd0f96abb66d8dee82925daf46"
         ),
         .target(
             name: "AMAShareUltralightBinary",
